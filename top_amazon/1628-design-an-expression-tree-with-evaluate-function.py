927545f065838ad6dcbfe86c3a0542868c672730
Marcos <marcosdp12@gmail.com>
2021-10-22 21:11:26 -0300

    more amazon questions

---
+++ top_amazon/1628-design-an-expression-tree-with-evaluate-function.py
@@ -0,0 +1,228 @@
+"""
+Given the postfix tokens of an arithmetic expression, build and return the binary expression tree that represents this expression.
+
+Postfix notation is a notation for writing arithmetic expressions in which the operands (numbers) appear before their operators. 
+For example, the postfix tokens of the expression 4*(5-(7+2)) are represented in the array postfix = ["4","5","7","2","+","-","*"].
+
+The class Node is an interface you should use to implement the binary expression tree. The returned tree will be tested using the 
+evaluate function, which is supposed to evaluate the tree's value. You should not remove the Node class; however, you can modify 
+it as you wish, and you can define other classes to implement it if needed.
+
+A binary expression tree is a kind of binary tree used to represent arithmetic expressions. Each node of a binary expression tree 
+has either zero or two children. Leaf nodes (nodes with 0 children) correspond to operands (numbers), and internal nodes 
+(nodes with two children) correspond to the operators '+' (addition), '-' (subtraction), '*' (multiplication), and '/' (division).
+
+It's guaranteed that no subtree will yield a value that exceeds 109 in absolute value, and all the operations are valid (i.e., no division by zero).
+
+Follow up: Could you design the expression tree such that it is more modular? For example, is your design able to support additional 
+operators without making changes to your existing evaluate implementation?
+
+Example 1:
+Input: s = ["3","4","+","2","*","7","/"]
+Output: 2
+Explanation: this expression evaluates to the above binary tree with expression ((3+4)*2)/7) = 14/7 = 2.
+
+Example 2:
+Input: s = ["4","5","2","7","+","-","*"]
+Output: -16
+Explanation: this expression evaluates to the above binary tree with expression 4*(5-(2+7)) = 4*(-4) = -16.
+
+Example 3:
+Input: s = ["4","2","+","3","5","1","-","*","+"]
+Output: 18
+
+Example 4:
+Input: s = ["100","200","+","2","/","5","*","7","+"]
+Output: 757
+ 
+Constraints:
+1 <= s.length < 100
+s.length is odd.
+s consists of numbers and the characters '+', '-', '*', and '/'.
+If s[i] is a number, its integer representation is no more than 105.
+It is guaranteed that s is a valid expression.
+The absolute value of the result and intermediate values will not exceed 109.
+It is guaranteed that no expression will include division by zero.
+"""
+import abc 
+from abc import ABC, abstractmethod 
+
+class Node(ABC):
+    @abstractmethod
+    def evaluate(self) -> int:
+        pass
+    
+class NumberNode(Node):
+    def __init__(self, data):
+        self.data = int(data)
+        self.left = None
+        self.right = None
+        
+    def evaluate(self):
+        return self.data
+    
+class OperationNode(Node):
+    def __init__(self, data, left = None, right = None):
+        self.data = data
+        self.left = left
+        self.right = right
+        
+    def evaluate(self):
+        if self.data == '+':
+            return self.left.evaluate() + self.right.evaluate() 
+        elif self.data == '-':
+            return self.left.evaluate() - self.right.evaluate()
+        elif self.data == '*':
+            return self.left.evaluate() * self.right.evaluate()
+        elif self.data == '/':
+            return int(self.left.evaluate() / self.right.evaluate())
+    
+    
+"""    
+This is the TreeBuilder class.
+You can treat it as the driver code that takes the postinfix input
+and returns the expression tree represnting it as a Node.
+"""
+
+class TreeBuilder(object):
+    def buildTree(self, postfix: List[str]) -> 'Node':
+        self.curr = len(postfix) - 1
+        
+        def recursiveBuild():
+            if self.curr < 0:
+                return
+            
+            data = postfix[self.curr]
+            self.curr -= 1
+            if data not in ["+","/","-","*"]:
+                return NumberNode(data)
+            
+            right = recursiveBuild()
+            left = recursiveBuild()
+            
+            return OperationNode(data, left, right)
+        
+        return recursiveBuild()
+            
+"""
+Your TreeBuilder object will be instantiated and called as such:
+obj = TreeBuilder();
+expTree = obj.buildTree(postfix);
+ans = expTree.evaluate();
+"""
+
+import abc 
+from abc import ABC, abstractmethod 
+"""
+This is the interface for the expression tree Node.
+You should not remove it, and you can define some classes to implement it.
+"""
+
+class Node(ABC):
+    @abstractmethod
+    # define your fields here
+    def evaluate(self) -> int:
+        pass
+    
+class NumericNode(Node):
+    # Must be a leaf node
+    def __init__(self, num):
+        self.num = num
+        
+    def evaluate(self):
+        return int(self.num)
+
+    
+class OperatorNode(Node):
+    # Must have 2 children
+    def __init__(self):
+        self.left = None
+        self.right = None
+        
+    def evaluate(self):
+        raise NotImplementedError
+        
+        
+class MultiplactionNode(OperatorNode):
+    def __init__(self):
+        super().__init__()
+    
+    def evaluate(self):
+        return self.left.evaluate() * self.right.evaluate()
+
+    
+class AdditionNode(OperatorNode):
+    def __init__(self):
+        super().__init__()
+    
+    def evaluate(self):
+        return self.left.evaluate() + self.right.evaluate()
+
+    
+class SubtractionNode(OperatorNode):
+    def __init__(self):
+        super().__init__()
+    
+    def evaluate(self):
+        return self.left.evaluate() - self.right.evaluate()
+
+    
+class DivisionNode(OperatorNode):
+    def __init__(self):
+        super().__init__()
+    
+    def evaluate(self):
+        return self.left.evaluate() // self.right.evaluate()
+    
+    
+class NodeFactory:
+    @staticmethod
+    def new_node(element) -> Node:
+        if isinstance(element, Node):
+            return element
+        
+        elif element.isdigit():
+            return NumericNode(element)
+        
+        else:
+            if element  == "+":
+                return AdditionNode()
+            
+            elif element == "-":
+                return SubtractionNode()
+                
+            elif element == "*":
+                return MultiplactionNode()
+                
+            else:
+                return DivisionNode()
+
+"""    
+This is the TreeBuilder class.
+You can treat it as the driver code that takes the postinfix input
+and returns the expression tree represnting it as a Node.
+"""
+
+class TreeBuilder(object):
+    def buildTree(self, postfix: List[str]) -> 'Node':
+        stack = []
+        
+        for value in postfix:
+            if not value.isdigit():
+                node = NodeFactory.new_node(value)
+                node.right = NodeFactory.new_node(stack.pop())
+                node.left = NodeFactory.new_node(stack.pop())
+                
+                stack.append(node)
+            else:
+                stack.append(value)
+                
+        return stack.pop()
+            
+            
+"""
+Your TreeBuilder object will be instantiated and called as such:
+obj = TreeBuilder();
+expTree = obj.buildTree(postfix);
+ans = expTree.evaluate();
+"""
---
+++ top_amazon/829-consecutive-numbers-sum.py
@@ -0,0 +1,35 @@
+"""
+Given an integer n, return the number of ways you can write n as the sum of consecutive positive integers.
+
+Example 1:
+Input: n = 5
+Output: 2
+Explanation: 5 = 2 + 3
+
+Example 2:
+Input: n = 9
+Output: 3
+Explanation: 9 = 4 + 5 = 2 + 3 + 4
+Example 3:
+
+Input: n = 15
+Output: 4
+Explanation: 15 = 8 + 7 = 4 + 5 + 6 = 1 + 2 + 3 + 4 + 5
+ 
+Constraints:
+
+1 <= n <= 109
+"""
+# N = (x + 1) + ... + (x + k)
+# N = x k + k(k + 1)/2
+class Solution:
+    def consecutiveNumbersSum(self, N: int) -> int:
+        count = 0
+        # x > 0 --> N/k - (k + 1)/2 > 0
+        upper_limit = ceil((2 * N + 0.25)**0.5 - 0.5) + 1
+        for k in range(1, upper_limit):
+            # x should be integer
+            if (N - k * (k + 1) // 2) % k == 0:
+                count += 1
+        return count
+        
