8b92b5bc734d002d9e18041f06e3e03a45fa940a
Marcos <marcosdp12@gmail.com>
2021-11-01 16:48:57 -0300

    update in Amazon questions

--- top_amazon/1135-connecting-cities-with-minimum-cost.py
+++ top_amazon/1135-connecting-cities-with-minimum-cost.py
@@ -42,7 +42,7 @@ class Solution:
             rootA = find(a)
             rootB = find(b)
             
-            if rootA == rootB: return;
+            if rootA == rootB: return
             
             if weights[rootA] > weights[rootB]:
                 parents[rootB] = rootA
@@ -66,3 +66,79 @@ class Solution:
             return total_cost
         return -1
         
+
+
+# # A Python program for Prim's Minimum Spanning Tree (MST) algorithm.
+# # The program is for adjacency matrix representation of the graph
+ 
+# import sys # Library for INT_MAX
+ 
+class Solution:
+  def primsAlgorithm(self, n, connections):
+    
+
+
+
+# class Graph():
+ 
+#     def __init__(self, vertices):
+#         self.V = vertices
+#         self.graph = [[0 for column in range(vertices)]
+#                     for row in range(vertices)]
+ 
+#     # A utility function to find the vertex with
+#     # minimum distance value, from the set of vertices
+#     # not yet included in shortest path tree
+#     def minKey(self, key, mstSet):
+#         min = sys.maxint
+#         for v in range(self.V):
+#             if key[v] < min and mstSet[v] == False:
+#                 min = key[v]
+#                 min_index = v
+ 
+#         return min_index
+ 
+#     def primMST(self):
+
+#         key = [sys.maxint] * self.V
+#         parent = [None] * self.V 
+#         key[0] = 0
+#         mstSet = [False] * self.V
+#         parent[0] = -1 
+ 
+#         for cout in range(self.V):
+ 
+#             # Pick the minimum distance vertex from
+#             # the set of vertices not yet processed.
+#             # u is always equal to src in first iteration
+#             u = self.minKey(key, mstSet)
+ 
+#             # Put the minimum distance vertex in
+#             # the shortest path tree
+#             mstSet[u] = True
+ 
+#             # Update dist value of the adjacent vertices
+#             # of the picked vertex only if the current
+#             # distance is greater than new distance and
+#             # the vertex in not in the shortest path tree
+#             for v in range(self.V):
+ 
+#                 # graph[u][v] is non zero only for adjacent vertices of m
+#                 # mstSet[v] is false for vertices not yet included in MST
+#                 # Update the key only if graph[u][v] is smaller than key[v]
+#                 if self.graph[u][v] > 0 and mstSet[v] == False and key[v] > self.graph[u][v]:
+#                         key[v] = self.graph[u][v]
+#                         parent[v] = u
+ 
+#         self.printMST(parent)
+ 
+# g = Graph(5)
+# g.graph = [ [0, 2, 0, 6, 0],
+#             [2, 0, 3, 8, 5],
+#             [0, 3, 0, 0, 7],
+#             [6, 8, 0, 0, 9],
+#             [0, 5, 7, 9, 0]]
+ 
+# g.primMST();
+ 
+# # Contributed by Divyanshu Mehta
--- top_amazon/23-merge-k-sorted-lists.py
+++ top_amazon/23-merge-k-sorted-lists.py
@@ -37,7 +37,7 @@ class Solution:
             last.next = curr
             last = curr
         
-        return merged.next
+        return merged.next    
 
 class Tests(unittest.TestCase):
 	def test_1(self):
@@ -54,3 +54,49 @@ class Tests(unittest.TestCase):
 
 if __name__ == '__main__':
 	unittest.main()
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# class Solution:
+#     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+#         if lists == None or len(lists) == 0:
+#             return None
+        
+#         amount = len(lists)
+#         interval = 1
+#         while interval < amount:
+#             for i in range(0, amount - interval, interval * 2):
+#                 lists[i] = self.merge2Lists(lists[i], lists[i + interval])
+#             interval *= 2
+#         return lists[0] if amount > 0 else None
+            
+#         return lists[0]
+
+#     def merge2Lists(self, l1, l2):
+#         if not l1: return l2
+#         if not l2: return l1
+        
+#         l = ListNode('#')
+#         runner = l
+        
+#         while l1 and l2:
+#             if l1.val < l2.val:
+#                 node = l1
+#                 l1 = l1.next
+#             else:
+#                 node = l2
+#                 l2 = l2.next
+#             node.next = None
+#             runner.next = node
+#             runner = runner.next
+            
+#         if l1:
+#             runner.next = l1
+#         if l2:
+#             runner.next = l2
+            
+#         return l.next
